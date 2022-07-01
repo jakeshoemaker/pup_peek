52b6cddde9288db41ff7f14e1edbb2f3570ef900
jake shoemaker <jakeshoe3@gmail.com>
2022-06-30 23:48:56 -0500

    barebones server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+    
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hi.html").unwrap();
+
+    let res = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+
+    stream.write(res.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
